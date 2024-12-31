0f025dfc4a9f190169b9c9271c6406407591174b
Roni Twito <ronitwito08@gmail.com>
2024-12-31 14:57:53 +0200

    messages tests and model

---
+++ backend-cwc/models/messages_model.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+export interface MessageInterface{
+    messageId?: string;
+    senderId: string;
+    receiverId: string;
+    content: string;
+    createdAt?: Date;
+    messageRead: boolean;
+}
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: String,
+    required: true,
+  },
+    receiverId: {
+        type: String,
+        required: true,
+    },
+    content:{
+        type: String,
+        required: true,
+    },
+    messageRead:{
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
+
