e187c87a8258d8868ef2346b68550a561e49c59b
omerboud <omerboud7712@gmail.com>
2024-12-31 11:39:55 +0200

    add comments controller and routes

---
+++ backend-cwc/routes/comment_routes.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import CommentControllers from "../controllers/comment_controller";
+import { Router } from "express";
+import { authMiddleware } from "../controllers/auth_controller";
+
+const router = express.Router();
+
+router.post("/createComment", authMiddleware, CommentControllers.createComment);
+
+router.get("/getComments", CommentControllers.getComments);
+
+router.get("/getCommentById/:commentId", CommentControllers.getCommentById);
+
+router.get("/getCommentByPostId/:postId", authMiddleware, CommentControllers.getCommentsByPostId);
+
+router.put("/updateComment/:commentId", authMiddleware, CommentControllers.updateComment);
+
+router.delete("/deleteComment/:commentId", authMiddleware, CommentControllers.deleteComment);
+
+export default router;
