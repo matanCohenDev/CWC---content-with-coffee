7a332d2eacba87ac5c285e7dd03931138e95f39a
Roni Twito <ronitwito08@gmail.com>
2025-01-14 20:27:45 +0200

    landing page and forms button and routes

--- frontend-cwc/src/App.tsx
+++ frontend-cwc/src/App.tsx
@@ -1,9 +1,15 @@
-import FormsPage from "./pages/Forms-page/FormsPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing-page/landing-page";
+import FormsPage from "./pages/Forms-page/FormsPage";
+
 function App() {
   return (
-      <FormsPage/>
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/forms" element={<FormsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend-cwc/src/pages/landing-page/components/Header/Header.tsx
+++ frontend-cwc/src/pages/landing-page/components/Header/Header.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";  
 import HeaderStyle from "./Header.module.css";
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+      navigate("/forms"); 
+    };
 
     return (
         <header className={HeaderStyle.header}>
@@ -25,10 +32,17 @@ export default function Header() {
                 <div className={HeaderStyle.hanburgerAndLogin}>
                     {/* Login Button */}
                     <div className={HeaderStyle.LogInSignUpContainer}>
-                        <button className={HeaderStyle.userButton}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <button 
+                            className={HeaderStyle.userButton}
+                            onClick={handleLoginClick}   
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" 
+                                 height="24" viewBox="0 0 24 24" fill="none" 
+                                 stroke="currentColor" strokeWidth="2" 
+                                 strokeLinecap="round" strokeLinejoin="round">
                                 <circle cx="12" cy="7" r="4"></circle>
-                                <path d="M5 21v-2a4 4 0 0 1 4-4h6a4 4 0 0 1 4 4v2"></path>
+                                <path d="M5 21v-2a4 4 0 0 1 4-4h6
+                                         a4 4 0 0 1 4 4v2"></path>
                             </svg>
                         </button>
                     </div>
