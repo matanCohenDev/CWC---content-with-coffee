7b83a7b490ea5e2bb6e068f32e225a3fa5818360
Roni Twito <ronitwito08@gmail.com>
2025-02-23 19:05:05 +0200

    upload post

--- frontend-cwc/src/pages/landing-page/landing-page.tsx
+++ frontend-cwc/src/pages/landing-page/landing-page.tsx
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import LandingPageStyle from "./landing-page.module.css";
 import Footer from "./components/Footer/Footer";
+import PostUpload from "../post-upload/uploadPost";
 
 export default function LandingPage() {
     return (
@@ -12,6 +13,7 @@ export default function LandingPage() {
             <Header />
             <Hero />
             <Description />
+            <PostUpload />
             <CoffeeExperience />
             <ContactUs />
             <Footer />
